37a0951ba85d1644e0fec57eec385e6b084fe19a
anhnguyen <anhn32616@gmail.com>
2022-10-14 08:55:06 +0700

    fix crud

--- src/routes/doctor.js
+++ src/routes/doctor.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 const doctorController = require('../controller/DoctorController');
 const authJwt = require('../middlewares/authJwt');
+const fileUploader = require('../config/cloudinary.config');
 router.get('/', doctorController.getAllDoctor);
 router.get('/:id', doctorController.getDoctorById);
-// router.post('/', authJwt.authenToken, authJwt.isAdmin, doctorController.createDoctor);
-// router.put('/:id', authJwt.authenToken, authJwt.isAdminOrYourself , doctorController.updateDoctor);
-// router.delete('/:id', authJwt.authenToken, authJwt.isAdmin, doctorController.deleteDoctor);
+router.post('/', authJwt.authenToken, authJwt.isAdmin, fileUploader.single('image'), doctorController.createDoctor);
+router.put('/:id', authJwt.authenToken, authJwt.isAdminOrYourself, fileUploader.single('image'), doctorController.updateDoctor);
+router.delete('/:id', authJwt.authenToken, authJwt.isAdmin, doctorController.deleteDoctor);
 
-router.post('/', doctorController.createDoctor);
-router.post('/', doctorController.createDoctor);
-router.put('/:id', doctorController.updateDoctor);
-router.delete('/:id', doctorController.deleteDoctor);
+// router.post('/', doctorController.createDoctor);
+// router.post('/', doctorController.createDoctor);
+// router.put('/:id', doctorController.updateDoctor);
+// router.delete('/:id', doctorController.deleteDoctor);
 module.exports = router;
