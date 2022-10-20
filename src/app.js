0cc777c637811c8fcfe8686ed7df230327e72606
anhnguyen <anhn32616@gmail.com>
2022-10-21 00:55:24 +0700

    add module cors

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-// const cors=require("cors");
-// const corsOptions ={
-//    origin:'*', 
-//    credentials:true,            //access-control-allow-credentials:true
-//    optionSuccessStatus:200,
-// }
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
-// app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.use(bodyParser.json())
 
