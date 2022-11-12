553d7909e6c0e55e273b7912d82cb2cd7df1ed33
= <=>
2022-11-13 01:45:09 +0700

    Get List User Chat And Event Chat

--- socketServer.js
+++ socketServer.js
@@ -1,42 +1,15 @@
 let users = []
 
-const EditData = (data, id, call) => {
-    const newData = data.map(item => 
-        item.id === id ? {...item, call} : item
-    )
-    return newData;
-}
-
 const SocketServer = (socket) => {
     // Connect - Disconnect
-    // socket.on('joinUser', user => {
-    //     users.push({id: user._id, socketId: socket.id, followers: user.followers})
-    // })
+    socket.on('joinUser', user => {
+        users.push({id: user._id, socketId: socket.id})
+        socket.emit("getUsers", users);
+    })
 
-    // socket.on('disconnect', () => {
-    //     const data = users.find(user => user.socketId === socket.id)
-    //     if(data){
-    //         const clients = users.filter(user => 
-    //             data.followers.find(item => item._id === user.id)
-    //         )
-
-    //         if(clients.length > 0){
-    //             clients.forEach(client => {
-    //                 socket.to(`${client.socketId}`).emit('CheckUserOffline', data.id)
-    //             })
-    //         }
-
-    //         if(data.call){
-    //             const callUser = users.find(user => user.id === data.call)
-    //             if(callUser){
-    //                 users = EditData(users, callUser.id, null)
-    //                 socket.to(`${callUser.socketId}`).emit('callerDisconnect')
-    //             }
-    //         }
-    //     }
-
-    //     users = users.filter(user => user.socketId !== socket.id)
-    // })
+    socket.on('disconnect', () => {
+        users = users.filter(user => user.socketId !== socket.id)
+    })
 
 
     // Notification
@@ -53,10 +26,10 @@ const SocketServer = (socket) => {
 
 
     // Message
-    // socket.on('addMessage', msg => {
-    //     const user = users.find(user => user.id === msg.recipient)
-    //     user && socket.to(`${user.socketId}`).emit('addMessageToClient', msg)
-    // })
+    socket.on('addMessage', msg => {
+        const user = users.find(user => user.id === msg.to_user)
+        user && socket.to(`${user.socketId}`).emit('addMessageToClient', msg)
+    })
 
 }
 
--- src/controller/MessagechatController.js
+++ src/controller/MessagechatController.js
@@ -7,14 +7,32 @@ let addMessage = async (req,res) => {
     } else{
         req.body.image=req.file.path;
     }
+    if (!req.body.from_user || !req.body.to_user || !req.body.text  ) {
+        return res.status(400).json({
+            erroCode:1,
+            message:'nhap day du thong tin'
+        })
+    }
     console.log(req.body)
     let message = await messageService.addMessage(req.body);
     return res.status(200).json(message);
 }
 
 let getListMessageChat = async (req,res) => {
-    let messages = await messageService.getListMessage(req.query)
-    return res.status(200).json(messages)
+    let pageNumber = req.query.page === undefined ? 0: req.query.page
+    let size = req.query.size === undefined ? 10 : req.query.size
+    let resData = await messageService.getListMessage(req.query,pageNumber,size)
+    let page ={}
+    page.size= resData.size
+    page.totalPages= resData.totalPages
+    page.totalElements = resData.totalElements
+    page.page = resData.page
+    return res.status(200).json({
+        erroCode:0,
+        message: 'OK',
+        messages: resData.messages,
+        page: page
+    })
 }
 
 module.exports ={
