5dca35ee5ea030f0e45876229033621d25e3fe7c
= <nguyenduyhoa30052001@gmail.com>
2022-12-23 07:55:16 +0700

    valiadate password and birthday

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const AuthService = require('../service/AuthService');
 const userService= require('../service/UserService');
 const patientService=require('../service/PatientService');
+const moment =require('moment');
 
 const multer = require('multer')
 const path = require('path')
@@ -52,7 +53,19 @@ let singup = async (req,res)=>{
     if (!req.body.email || !req.body.password || !req.body.firsname || !req.body.lastname || !req.body.gender || !req.body.phoneNumber || !req.body.birthday || !req.body.address  ) {
         return res.status(400).json({
             erroCode:1,
-            message:'nhap day du thong tin'
+            message:'nhập đầy đủ thông tin'
+        })
+    }
+    if(req.body.password.length < 5 || req.body.password.length>15 ){
+        return res.status(400).json({
+            erroCode:1,
+            message:'độ dài mật khẩu phải lớn hơn 5 ký tự và không quá 15 ký tự'
+        })
+    }
+    if(!moment(req.body.birthday, 'YYYY-MM-DD',true).isValid()){
+        return res.status(400).json({
+            erroCode:1,
+            message:'định dạng birthday không đúng. Ví dụ về định dạng đúng : 2022-11-20'
         })
     }
     if (!req.file){
