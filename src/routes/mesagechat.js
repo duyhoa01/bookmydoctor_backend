3fbed95faf41d72aa0e0cf1e471fb00f6a7af432
= <nguyenduyhoa30052001@gmail.com>
2022-12-26 13:16:42 +0700

    fix auth

--- src/routes/mesagechat.js
+++ src/routes/mesagechat.js
@@ -5,8 +5,8 @@ const messageController = require('../controller/MessagechatController');
 const fileUploader = require('../config/cloudinary.config');
 const authJwt = require('../middlewares/authJwt')
 
-router.post('/',fileUploader.single('image'),messageController.addMessage);
-router.get('/',messageController.getListMessageChat);
+router.post('/',authJwt.authenToken,fileUploader.single('image'),messageController.addMessage);
+router.get('/',authJwt.authenToken,messageController.getListMessageChat);
 
 
 module.exports = router
